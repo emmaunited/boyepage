3571d2ea5e1d176045782df2688d410ea26256f6
Emmanuel Ajiboye <eoa22@njit.edu>
2018-10-18 15:49:04 -0400

    Task: First layout of website is completed

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,6 +2,9 @@
 @section('content')
     <br>
     <br>
+    <br>
+    <h2>Our mission</h2>
 
-    <h1>Hello this is about page</h1>
+    <p>To bring the up-to-date news about your favorite sports, players and fantasy games.
+        Our main focus is to inspire every fan and bring excitement to every fan in the world.</p>
 @stop
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,36 @@
     <br>
     <br>
 
-    <h1>Hello this is boyepage</h1>
+
+    <!-- Jumbotron -->
+    <div class="jumbotron">
+        <h1>Sport cast!</h1>
+        <p class="lead">We work 24/7 to keep you up-to-date with the latest sport news (soccer, football, nhl, mlb etc.)
+            In other to access the page, please Sign-in or Sign-up.
+        </p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
+    </div>
+
+    <!-- Example row of columns -->
+    <div class="row">
+        <div class="col-lg-4">
+            <h2>News</h2>
+            <b>Josh Gordon's tenure in Cleveland ends with trade to Patriots</b>
+            <p>The Cleveland Browns have traded talented but troubled wide receiver Josh Gordon to the New England Patriots: In return, the Browns will get a 2019 fifth round pick. </p>
+            <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+            <h2>Videos</h2>
+            <p>As week 2 come to an End, here are the top play of the weekend. </p>
+            <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+            <h2>Fantasy</h2>
+            <b>NFL Week 3 Power Rankings: Rams are new No. 1... Chaos below</b>
+            <p> There's turnover at the top of the Power Rankings, with new teams cracking the top three. Plus: Why every team can be optimistic</p>
+            <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+    </div>
+
 
 @stop
