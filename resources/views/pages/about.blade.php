477226be66651c51d5cb8428f61ab59b3f395986
Emmanuel Ajiboye <eoa22@njit.edu>
2018-10-18 15:20:07 -0400

    Laravel Setup

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('content')
+    <br>
+    <br>
+
+    <h1>Hello this is about page</h1>
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+@section('content')
+    <br>
+    <br>
+
+    <h1>Hello this is boyepage</h1>
+
+@stop
